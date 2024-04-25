b703e911939a678babab1ac1d793e7adc52be89a
Anna Kennedy <annakennedy@eduroam-int-dhcp-97-154-49.ucl.ac.uk>
2024-04-25 11:00:29 +0100

    first commit

---
+++ Documents/Work/MONICA/processing/event.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+
+def find_num_frames(tracking_filename):
+    tracking_db = pd.read_hdf(tracking_filename)
+    num_frames = tracking_db.shape[0]
+    return num_frames
+
+def get_stim_events(stim):
+    
+    stim_int = stim.astype(np.int8) 
+    stim_event_frames = []
+    
+    for index, value in enumerate(stim_int):
+        if index == 0:
+            prev_value = 0
+        else:
+            flip = value - prev_value
+            if flip == 1:
+                stim_event_frames.append(index)
+            prev_value = value
+            
+    return stim_event_frames
+
+def conform_stim(stim, num_frames):
+    
+    stim_length = stim.size
+    disparity = num_frames - stim_length
+    
+    if disparity > 0:
+        conformed_stim = np.append(stim, np.zeros(disparity, dtype=bool))
+        
+    elif disparity < 0:
+        conformed_stim = stim[:disparity]
+        
+    else:
+        conformed_stim = stim
+        
+    return conformed_stim
+
+def read_stim_file(stim_file, num_frames):
+    
+    stim_tracking = pd.read_csv(stim_file)
+    raw_stim = np.array(stim_tracking.to_numpy().flatten(), dtype=bool)
+    
+    stim = conform_stim(raw_stim, num_frames)
+    
+    return stim
---
+++ Documents/Work/MONICA/run.py
@@ -0,0 +1,36 @@
+import sys
+from utils import parse, files
+from classes.angle_plots import AnglePlots
+
+def run():
+
+    TRACKING_FILE = parse_args()
+    STIM_FILE = files.load_stim_file(TRACKING_FILE)
+    VIDEO_FILE = files.load_video_file(TRACKING_FILE)
+
+    ap = AnglePlots(TRACKING_FILE, STIM_FILE, VIDEO_FILE)
+    ap.process_data(STIM_FILE, VIDEO_FILE)
+    ap.load_stim_file(STIM_FILE)
+    ap.draw_global_plots()
+    ap.draw_event_plots()
+    ap.save_angles()
+    ap.save_pdf_report()
+
+def parse_args():
+
+    if len(sys.argv) == 1:
+        
+        raise KeyError("tracking file must be specified")
+    
+    elif len(sys.argv) == 2:
+        
+        folder = parse.h5_file(sys.argv[1])
+            
+    else:
+        
+        raise KeyError("Too many input arguments")
+    
+    return folder
+
+if __name__ == "__main__":
+    run()
